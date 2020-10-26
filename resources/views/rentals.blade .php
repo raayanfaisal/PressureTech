2a12705744cc734e75159bf84527a19f0f69dbb9
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-10-26 16:35:01 +0500

    Update rentals.blade .php

--- resources/views/rentals.blade .php
+++ resources/views/rentals.blade .php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-4 col-sm-6">
             <div class="product-grid2">
-                <div class="product-image2"> <a href="#"> <img class="pic-1" src="/images/one.jpg"> <img class="pic-2" src="/images/one.jpg"> </a>
+                <div class="product-image2"> <a href="#"> <img class="pic-1" src="/images/compressor-coltri.png"> <img class="pic-2" src="/images/compressor-coltri.png"> </a>
                     <ul class="social">
                         <li><a href="#" data-tip="Quick View"><i class="fa fa-eye"></i></a></li>
                         <li><a href="#" data-tip="Add to Cart"><i class="fa fa-shopping-cart"></i></a></li>
@@ -24,10 +24,10 @@
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Modal Header1</h4>
+        <h4 class="modal-title">COLTRI Compressor</h4>
       </div>
       <div class="modal-body">
-        <p>Some text in the modal.</p>
+        <p>Branded High pressure breathing air compressors</p>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
@@ -38,7 +38,7 @@
 </div>
         <div class="col-md-4 col-sm-6">
             <div class="product-grid2">
-                <div class="product-image2"> <a href="#"> <img class="pic-1" src="/images/two.jpg"> <img class="pic-2" src="/images/two.jpg"> </a>
+                <div class="product-image2"> <a href="#"> <img class="pic-1" src="/images/cylinders-diving.jpg"> <img class="pic-2" src="/images/cylinders-diving.jpg"> </a>
                     <ul class="social">
                         <li><a href="#" data-tip="Quick View"><i class="fa fa-eye"></i></a></li>
                         <li><a href="#" data-tip="Add to Cart"><i class="fa fa-shopping-cart"></i></a></li>
@@ -57,10 +57,10 @@
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Modal Header2</h4>
+        <h4 class="modal-title">Diving Cylinder</h4>
       </div>
       <div class="modal-body">
-        <p>Some text in the modal.</p>
+        <p>Diving cylinders and Oxygen Kits</p>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
@@ -71,7 +71,7 @@
 </div>
         <div class="col-md-4 col-sm-6">
             <div class="product-grid2">
-                <div class="product-image2"> <a href="#"> <img class="pic-1" src="/images/three.jpg"> <img class="pic-2" src="/images/three.jpg"> </a>
+                <div class="product-image2"> <a href="#"> <img class="pic-1" src="/images/regulator-bcd-set.jpeg"> <img class="pic-2" src="/images/regulator-bcd-set.jpeg"> </a>
                     <ul class="social">
                         <li><a href="#" data-tip="Quick View"><i class="fa fa-eye"></i></a></li>
                         <li><a href="#" data-tip="Add to Cart"><i class="fa fa-shopping-cart"></i></a></li>
@@ -91,10 +91,10 @@
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Modal Header3</h4>
+        <h4 class="modal-title">Regulator BCD set</h4>
       </div>
       <div class="modal-body">
-        <p>Some text in the modal.</p>
+        <p>Complete dive gear </p>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
@@ -108,7 +108,7 @@
     <div class="row">
         <div class="col-md-4 col-sm-6">
             <div class="product-grid2">
-                <div class="product-image2"> <a href="#"> <img class="pic-1" src="/images/01.jpg"> <img class="pic-2" src="/images/01.jpg"> </a>
+                <div class="product-image2"> <a href="#"> <img class="pic-1" src="/images/san-o-sub-oxygen-rescue-set.jpg"> <img class="pic-2" src="/images/san-o-sub-oxygen-rescue-set.jpg"> </a>
                     <ul class="social">
                         <li><a href="#" data-tip="Quick View"><i class="fa fa-eye"></i></a></li>
                         <li><a href="#" data-tip="Add to Cart"><i class="fa fa-shopping-cart"></i></a></li>
@@ -127,10 +127,10 @@
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Modal Header4</h4>
+        <h4 class="modal-title">Oxygen Kits</h4>
       </div>
       <div class="modal-body">
-        <p>Some text in the modal.</p>
+        <p>Oxygen Kits</p>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
@@ -139,174 +139,7 @@
 
   </div>
 </div>
-        <div class="col-md-4 col-sm-6">
-            <div class="product-grid2">
-                <div class="product-image2"> <a href="#"> <img class="pic-1" src="/images/02.jpg"> <img class="pic-2" src="/images/02.jpg"> </a>
-                    <ul class="social">
-                        <li><a href="#" data-tip="Quick View"><i class="fa fa-eye"></i></a></li>
-                        <li><a href="#" data-tip="Add to Cart"><i class="fa fa-shopping-cart"></i></a></li>
-                    </ul>
-                </div>
-                <div class="product-content">
-                <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#six">See details.</button>
-                </div>
-            </div>
-        </div>
-<!-- Modal -->
-<div id="six" class="modal fade" role="dialog">
-  <div class="modal-dialog">
 
-    <!-- Modal content-->
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Modal Header5</h4>
-      </div>
-      <div class="modal-body">
-        <p>Some text in the modal.</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-
-  </div>
-</div>
-        <div class="col-md-4 col-sm-6">
-            <div class="product-grid2">
-                <div class="product-image2"> <a href="#"> <img class="pic-1" src="/images/03.jpg"></a>
-                    <ul class="social">
-                        <li><a href="#" data-tip="Quick View"><i class="fa fa-eye"></i></a></li>
-                        <li><a href="#" data-tip="Add to Cart"><i class="fa fa-shopping-cart"></i></a></li>
-                    </ul>
-                </div>
-                <div class="product-content">
-                <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#seven">See details.</button>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Modal -->
-<div id="seven" class="modal fade" role="dialog">
-  <div class="modal-dialog">
-
-    <!-- Modal content-->
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Modal Header6</h4>
-      </div>
-      <div class="modal-body">
-        <p>Some text in the modal.</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-
-  </div>
-</div>
-    <div class="row">
-        <div class="col-md-4 col-sm-6">
-            <div class="product-grid2">
-                <div class="product-image2"> <a href="#"> <img class="pic-1" src="/images/two.jpg"> <img class="pic-2" src="/images/two.jpg"> </a>
-                    <ul class="social">
-                        <li><a href="#" data-tip="Quick View"><i class="fa fa-eye"></i></a></li>
-                        <li><a href="#" data-tip="Add to Cart"><i class="fa fa-shopping-cart"></i></a></li>
-                    </ul>
-                </div>
-                <div class="product-content">
-                <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#eight">See details.</button>
-                </div>
-            </div>
-        </div>
-        <!-- Modal -->
-<div id="eight" class="modal fade" role="dialog">
-  <div class="modal-dialog">
-
-    <!-- Modal content-->
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Modal Header7</h4>
-      </div>
-      <div class="modal-body">
-        <p>Some text in the modal.</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-
-  </div>
-</div>
-        <div class="col-md-4 col-sm-6">
-            <div class="product-grid2">
-                <div class="product-image2"> <a href="#"> <img class="pic-1" src="/images/five.jpg"> <img class="pic-2" src="/images/five.jpg"> </a>
-                    <ul class="social">
-                        <li><a href="#" data-tip="Quick View"><i class="fa fa-eye"></i></a></li>
-                        <li><a href="#" data-tip="Add to Cart"><i class="fa fa-shopping-cart"></i></a></li>
-                    </ul>
-                </div>
-                <div class="product-content">
-                <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#nine">See details.</button>
-                </div>
-            </div>
-        </div>
-        <!-- Modal -->
-<div id="nine" class="modal fade" role="dialog">
-  <div class="modal-dialog">
-
-    <!-- Modal content-->
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Modal Header8</h4>
-      </div>
-      <div class="modal-body">
-        <p>Some text in the modal.</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-
-  </div>
-</div>
-        <div class="col-md-4 col-sm-6">
-            <div class="product-grid2">
-                <div class="product-image2"> <a href="#"> <img class="pic-1" src="/images/one.jpg"> <img class="pic-2" src="/images/one.jpg"> </a>
-                    <ul class="social">
-                        <li><a href="#" data-tip="Quick View"><i class="fa fa-eye"></i></a></li>
-                        <li><a href="#" data-tip="Add to Cart"><i class="fa fa-shopping-cart"></i></a></li>
-                    </ul>
-                </div>
-                <div class="product-content">
-                    <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#ten">See details.</button>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Modal -->
-<div id="ten" class="modal fade" role="dialog">
-  <div class="modal-dialog">
-
-    <!-- Modal content-->
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Modal Header9</h4>
-      </div>
-      <div class="modal-body">
-        <p>Some text in the modal.</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-
-  </div>
-</div>
 </div>
 <section>
                <!-- Footer -->
