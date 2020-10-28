23a415d82ca013cce4258235cb85f39f6aeadf01
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-10-28 10:30:51 -0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -31,9 +31,12 @@
     <div class="heading-01">
         <h2 class="text-center m-5"><b>WHO WE ARE<span class="question-01">?</b><span></h2>    
     </div>
-        <p class="main-text">Pressure Tech Maldives Pvt Ltd was found in 2016 in a tiny office in Capital City Male’ and has grown to become a leading partner for Complete Scuba Diving Solutions and aftersales service for both High pressure & Low-Pressure machines equipment
-with a customer base of 80 Luxury resorts, 50 Liveaboards (Safari boats) over 50 Local Guesthouse dive operations and a vast majority of fishing vessels in the Maldives
-Pressure Tech Maldives is also the Exclusive distributor for Coltri compressors in the Maldives.
+        <p class="main-text">Pressure Tech Maldives Pvt Ltd was found in 2016 in a tiny office in Capital City Male’ and has
+grown to become a leading partner for Complete Scuba Diving Solutions and aftersales
+service for both High pressure &amp; Low-Pressure machines equipment with a customer base of
+80 Luxury resorts, 50 Liveaboards (Safari boats) over 50 Local Guesthouse dive operations and
+a vast majority of fishing vessels in the Maldives Pressure Tech Maldives is also the distributor
+for Coltri Sub Compressors in the Maldives.
 </p>
     </div>
 </section>
