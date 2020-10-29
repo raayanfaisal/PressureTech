b50d283c395fb4efa7649c68255298367f361b4d
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-10-29 19:03:35 +0500

    Update coltri.blade.php

--- resources/views/coltri.blade.php
+++ resources/views/coltri.blade.php
@@ -6,16 +6,13 @@
     <div class="heading-04">
         <h2><b>Coltri ERGO 13-16-21-23 TPS
 </b></h2>
-        <p>Line: Prime
-Compactness, performance, robustness and transportability
- are the main features of the ERGO TPS breathable air compressors. 
- Equipped with four different pumping units (MCH 13, MCH 16, MCH 21 and MCH 23),
-  all in TPS (Tropical Plus Superdry) version and powered by three-phase electric motors. 
-  The compressors have a monitoring system panel including pressure gauges, 
-  autostop with adjustable pressure switch, 
-  automatic condensate drain and and a phase control for rotation direction.
-   The models of this series have two connections for high pressure hoses (from 232 to 330 bar) 
-   and an improved filtering system with two Hyperfilters.</p>
+        <p>Frame: Mark 3 TPS - Line: Prime
+The MARK 3 series is a series of uncompromised breathable air 
+compressors for intensive use in warm, poorly ventilated or tropical environments. 
+Designed with an improved cooling system and a filtering system with two Hyperfilters, 
+this series is powered by a three-phase electric motor, equipped with four different pumping units 
+(MCH 13, MCH 16, MCH 21 and MCH 23), all in TPS (Tropical Plus Superdry) version and available with operation monitoring system and 
+electrical display to automatically shutdown the compressor.</p>
         <img src="" alt="" >
     </div>
 </div>
