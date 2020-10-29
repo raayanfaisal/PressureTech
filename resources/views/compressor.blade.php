2e025ee0c81b2efadfe88f25664ee116f3e1298f
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-10-29 14:58:40 +0500

    Update compressor.blade.php

--- resources/views/compressor.blade.php
+++ resources/views/compressor.blade.php
@@ -2,14 +2,14 @@
 
 @section('content')
 <div class="container">
-    <div class="heading-04">
-        <h2><b>COLTRI COMPRESSOR MCH 13/ MCH 16 SUPER SILENT EVO</b></h2>
-        <p>The Super Silent Evo series stands out for its high degree of silence. 
+        <img src="" alt="compressor" width="200px" height="200px">
+         <div class="heading-04">
+         <h2><b>COLTRI COMPRESSOR MCH 13/ MCH 16 SUPER SILENT EVO</b></h2>
+    </div>
+    <p>The Super Silent Evo series stands out for its high degree of silence. 
         This series was developed by Coltri to meet certain requirements, 
         such as easy to use, sound insulation and control systems that enable effective monitoring of the compressor. 
         The standard equipment includes two filling connections (from 232 to 330 bar), pressure gauge per stage, oil level monitoring, 
         internal temperature monitoring, direction of rotation monitoring, three-phase electric motor, automatic condensation and auto-stop. </p>
-        <img src="" alt="compressor" width="200px" height="200px">
-    </div>
 </div>
 @endsection
