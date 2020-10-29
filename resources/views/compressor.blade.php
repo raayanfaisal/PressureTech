98c1ea27104c85807a6495c011e750efc85dfd68
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-10-29 14:54:32 +0500

    Add files via upload

---
+++ resources/views/compressor.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="heading-04">
+        <h2><b>COLTRI COMPRESSOR MCH 13/ MCH 16 SUPER SILENT EVO</b></h2>
+        <p>The Super Silent Evo series stands out for its high degree of silence. This series was developed by Coltri to meet certain requirements , such as easy to use, sound insulation</p>
+        <img src="" alt="" >
+    </div>
+</div>
+@endsection
