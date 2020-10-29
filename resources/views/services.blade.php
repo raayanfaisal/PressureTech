1d7843540d89377a57b4edeef58a709feb3a2765
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-10-29 13:58:08 +0500

    Update services.blade.php

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -3,158 +3,30 @@
 @section('content')
 <div class="container">
     <div class="services-heading">
-        <h2 class="text-center"><b>OUR SERVICES</b></h2>
-        <P class="mt-5">Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-        when an unknown printer took a galley of type and scrambled it to make a type specimen book. </P>
+        <h2 class="text-center"><b>Servicing & maintenance of Breathing Air Compressors</b></h2>
+        <P class="mt-5">Our Technicians are trained to service all the products we sell. We can service your Coltri, 
+        Bauer and L&W compressor with confidence and timely manner. We are just a phone call away from your next compressor routine maintenance, 
+        troubleshooting or complete overhaul. </P>
     </div>
-    <div class="card-deck mt-5">
-  <div class="card">
-    <img class="card-img-top" src="/images/1.jpg" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title text-center"><b>Card title</b></h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    
+    <div class="services-heading">
+        <h2 class="text-center"><b>Dive Equipment Servicing</b></h2>
+        <P class="mt-5">Our in-house servicing team are ready and waiting to help with all our dive equipment servicing needs.  We service all major brand Regulators, BCDs, Dive Cylinders. 
+Equipment can be dropped off to our Service Center in Male’ City, or you can Send it to us using your preferred delivery method. If you want us to pickup your equipment, just give us a call or send us a message
+ </P>
     </div>
-    <div class="card-footer">
-      <small class="text-muted">Last updated 3 mins ago</small>
+    
+    <div class="services-heading">
+        <h2 class="text-center"><b>Cylinder Inspection to European Standards -BSEN1802 & 1968</b></h2>
+        <p>Visual Inspection & Hydro Testing of Diving cylinders ,Storage Cylinders ,Carbon Fiber Cylinders</p>
+        <ul>
+            <li>Luxfer UK Cylinders  & Luxfer USA cylinders</li>
+            <li>Catalina Cylinders</li>
+            <li>San O Sub  Medical Cylinders & Valves</li>
+            <li>Alcan Cylinders</li>
+            <li>Luxfer SCOTT CF cylinders</li>
+            <li>Faber Steel Cylinders</li>
+        </ul>
     </div>
-  </div>
-  <div class="card">
-    <img class="card-img-top" src="/images/1.jpg" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title text-center"><b>Card title</b></h5>
-      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-    </div>
-    <div class="card-footer">
-      <small class="text-muted">Last updated 3 mins ago</small>
-    </div>
-  </div>
-  <div class="card">
-    <img class="card-img-top" src="/images/1.jpg" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title text-center"><b>Card title</b></h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-    </div>
-    <div class="card-footer">
-      <small class="text-muted">Last updated 3 mins ago</small>
-    </div>
-  </div>
 </div>
-
-<div class="card-deck mt-5">
-  <div class="card">
-    <img class="card-img-top" src="/images/1.jpg" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title text-center"><b>Card title</b></h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    </div>
-    <div class="card-footer">
-      <small class="text-muted">Last updated 3 mins ago</small>
-    </div>
-  </div>
-  <div class="card">
-    <img class="card-img-top" src="/images/1.jpg" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title text-center"><b>Card title</b></h5>
-      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-    </div>
-    <div class="card-footer">
-      <small class="text-muted">Last updated 3 mins ago</small>
-    </div>
-  </div>
-  <div class="card">
-    <img class="card-img-top" src="/images/1.jpg" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title text-center"><b>Card title</b></h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-    </div>
-    <div class="card-footer">
-      <small class="text-muted">Last updated 3 mins ago</small>
-    </div>
-  </div>
-</div>
-</div>
-<section class="mt-5">
-               <!-- Footer -->
-        <footer class="page-footer font-small blue pt-4">
-        <div class="container">
-        <!-- Footer Links -->
-        <div class="container-fluid text-center text-md-left">
-
-        <!-- Grid row -->
-        <div class="row">
-
-            <!-- Grid column -->
-            <div class="col-md-6 mt-md-0 mt-3">
-
-            <!-- Content -->
-            <h5 class="text-uppercase"><b>MANDHOO DIVING</b></h5>
-
-
-            </div>
-            <!-- Grid column -->
-
-            <hr class="clearfix w-100 d-md-none pb-3">
-
-            <!-- Grid column -->
-            <div class="col-md-3 mb-md-0 mb-3">
-
-            <!-- Links -->
-            <h5 class="text-uppercase"><b>USEFULL LINKS</b></h5>
-
-            <ul class="list-unstyled">
-                <li>
-                <a href="#!">Back to top</a>
-                </li>
-                <li>
-                <a href="#!">Products</a>
-                </li>
-                <li>
-                <a href="#!">Contacts</a>
-                </li>
-            </ul>
-
-            </div>
-            <!-- Grid column -->
-
-            <!-- Grid column -->
-            <div class="col-md-3 mb-md-0 mb-3">
-
-            <!-- Links -->
-            <h5 class="text-uppercase"><b>SOCIAL LINKS</b></h5>
-
-            <ul class="list-unstyled">
-                <li>
-                <a href="#!">FACEBOOK</a>
-                </li>
-                <li>
-                <a href="#!">TWITTER</a>
-                </li>
-                <li>
-                <a href="#!">INSTAGRAM</a>
-                </li>
-                <li>
-                <a href="#!">LINKDNIN</a>
-                </li>
-            </ul>
-
-            </div>
-            <!-- Grid column -->
-
-        </div>
-        <!-- Grid row -->
-
-        </div>
-        <!-- Footer Links -->
-
-        <!-- Copyright -->
-        <div class="footer-copyright text-center py-3"><b>© 2020 Copyright:</b>
-        <a href="https://mdbootstrap.com/"> MANDHOO D</a>
-        </div>
-        <!-- Copyright -->
-        </div>
-        </footer>
-        <!-- Footer -->
-    </div>
-</section>
 @endsection
