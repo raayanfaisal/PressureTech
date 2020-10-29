47431584ac6fa4c1a4ceaf7d33b4a9a3c7d2419e
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-10-29 18:54:35 +0500

    Add files via upload

---
+++ resources/views/coltri.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+<img src="/images/xl-270420-081150-mch-13-16-18-et-compact-evo-front01-lr.jpg" alt="aircompressir" width="300px" height="200px">
+    <div class="heading-04">
+        <h2><b>Coltri ERGO 13-16-21-23 TPS
+</b></h2>
+        <p>Line: Prime
+Compactness, performance, robustness and transportability
+ are the main features of the ERGO TPS breathable air compressors. 
+ Equipped with four different pumping units (MCH 13, MCH 16, MCH 21 and MCH 23),
+  all in TPS (Tropical Plus Superdry) version and powered by three-phase electric motors. 
+  The compressors have a monitoring system panel including pressure gauges, 
+  autostop with adjustable pressure switch, 
+  automatic condensate drain and and a phase control for rotation direction.
+   The models of this series have two connections for high pressure hoses (from 232 to 330 bar) 
+   and an improved filtering system with two Hyperfilters.</p>
+        <img src="" alt="" >
+    </div>
+</div>
+@endsection
