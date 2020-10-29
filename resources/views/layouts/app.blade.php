8ecbff6b2076ca13f0a1ca08513422e8dc7bd095
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-10-29 12:38:36 +0500

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -301,7 +301,7 @@ ul#menu1 li {
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="navbar-brand" href="{{ url('/') }}" style="color: #006994;">
                     PRESSURETECH MALDIVES
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
