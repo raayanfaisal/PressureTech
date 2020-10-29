95e52ad047e9b9121e78db5684118c7b0adc9c45
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-10-29 20:17:46 +0500

    Add files via upload

---
+++ resources/views/cylinder.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+<img src="/images/Coltri MCH 8-11-13-16 SMART.png.jpg" alt="aircompressir" width="300px" height="200px">
+    <div class="heading-04">
+        <h2><b>Steel Diving Cylinder</b></h2>
+<p>We offer steel and aluminium cylinders  and Valves at a competitive price
+</p>
+
+        <img src="" alt="" >
+    </div>
+</div>
+@endsection
