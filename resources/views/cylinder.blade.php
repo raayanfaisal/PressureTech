cf56a3345f7ae335b41f61e7beaaedbe7ea930c9
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-10-29 20:21:28 +0500

    Update cylinder.blade.php

--- resources/views/cylinder.blade.php
+++ resources/views/cylinder.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-<img src="/images/Coltri MCH 8-11-13-16 SMART.png.jpg" alt="aircompressir" width="300px" height="200px">
+<img src="/images/Diving cylinder.png" alt="aircompressir" width="300px" height="200px">
     <div class="heading-04">
         <h2><b>Steel Diving Cylinder</b></h2>
 <p>We offer steel and aluminium cylinders  and Valves at a competitive price
