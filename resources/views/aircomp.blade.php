d789772699c56da0bb8b1bf82bac5c9b0345846a
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-10-29 18:32:55 +0500

    Update aircomp.blade.php

--- resources/views/aircomp.blade.php
+++ resources/views/aircomp.blade.php
@@ -2,10 +2,16 @@
 
 @section('content')
 <div class="container">
+<img src="/images/xl-270420-081150-mch-13-16-18-et-compact-evo-front01-lr.jpg" alt="aircompressir" width="300px" height="200px">
     <div class="heading-04">
         <h2><b>High pressure breathing air compressors
 Coltri Compact Evo</b></h2>
-        <p>The COMPACT EVO model has been designed with control systems that allow the effective monitoring of operations. The models of this line are equipped with two connections for high pressure tubes (from 232 to 330 bar), interstage pressure controls, oil level, internal cab temperature and rotation direction detector. They are equipped with three-phase motors and powerful pumping units (MCH 13, MCH 16 and MCH 18 depending on the specific model), automatic condensate drain and electric panel for management of the automatic shut-off of the compressor. The remote filling panel can be added as an option.
+        <p>The COMPACT EVO model has been designed with control systems that allow the effective monitoring of operations. 
+        The models of this line are equipped with two connections for high pressure tubes (from 232 to 330 bar), interstage pressure controls, 
+        oil level, internal cab temperature and rotation direction detector.
+        They are equipped with three-phase motors and powerful pumping units (MCH 13, MCH 16 and MCH 18 depending on the specific model), 
+        automatic condensate drain and electric panel for management of the automatic shut-off of the compressor.
+        The remote filling panel can be added as an option.
 </p>
         <img src="" alt="" >
     </div>
