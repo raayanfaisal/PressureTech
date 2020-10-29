6f1defd0cb2af9b50a2a629d11b91f75cbd83467
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-10-29 19:51:11 +0500

    Add files via upload

---
+++ resources/views/pete.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+<img src="/images/unnamed.jpg" alt="aircompressir" width="300px" height="200px">
+    <div class="heading-04">
+        <h2><b>Bauer PE-TE/PE-TB, 200-300l/min
+</b></h2>
+        <p>The transportable compressor units with optimised equipment for use in vehicles and on ships
+Uncompromising on quality: With a robust compressor block and piston rings made from wear-resistant special plastic
+Durable and robust: Thanks to oil cooling in the final compressor stage. For low wear and long service life
+Ideal for use at sea: Intercoolers and aftercoolers made from rust-free stainless steel. For high corrosion resistance
+Reliable lubrication: Thanks to low-pressure oil pump with oil filter. For effective lubrication and long oil change intervals
+</p>
+        <img src="" alt="" >
+    </div>
+</div>
+@endsection
