8ff2dfbc6528007bf00fe66ee72c489dfc21a2f9
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-10-29 20:24:57 +0500

    Add files via upload

---
+++ resources/views/gear.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+<img src="/images/Diving cylinder.png" alt="aircompressir" width="300px" height="200px">
+    <div class="heading-04">
+        <h2><b>Dive gear</b></h2>
+<p>We supply branded BCD, Regulator set and other Accessories with manufacturer warranty
+</p>
+
+        <img src="" alt="" >
+    </div>
+</div>
+@endsection
