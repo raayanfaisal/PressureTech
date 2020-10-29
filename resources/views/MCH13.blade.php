271cc7c5b6d017d0f5f634a393da22d2f9768a3b
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-10-29 19:13:49 +0500

    Add files via upload

---
+++ resources/views/MCH13.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+<img src="/images/xl-280420-175754-001-prime-line-ergo-tps-13-16-21-23-front01.jpg" alt="aircompressir" width="300px" height="200px">
+    <div class="heading-04">
+        <h2><b>Coltri MCH 13-16-21-23 Mark 3
+</b></h2>
+        <p>Frame: Mark 3 TPS - Line: Prime
+The MARK 3 series is a series of uncompromised breathable air compressors for intensive use in warm, poorly ventilated or tropical environments. Designed with an improved cooling system and a filtering system with two Hyperfilters, this series is powered by a three-phase electric motor, equipped with four different pumping units (MCH 13, MCH 16, MCH 21 and MCH 23), all in TPS (Tropical Plus Superdry) version and available with operation monitoring system and electrical display to automatically shutdown the compressor.</p>
+        <img src="" alt="" >
+    </div>
+</div>
+@endsection
