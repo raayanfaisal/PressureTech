23cde33f7f1cb973f7746843b5ec4de509f42401
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-10-29 15:06:25 +0500

    Update products.blade.php

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -17,24 +17,7 @@
             </div>
         </div>
         <!-- Modal -->
-<div id="one" class="modal fade" role="dialog">
-  <div class="modal-dialog">
 
-    <!-- Modal content-->
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">COLTRI Compressor</h4>
-      </div>
-      <div class="modal-body">
-        <p>Branded High pressure breathing air compressors</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-
-  </div>
 </div>
         <div class="col-md-4 col-sm-6">
             <div class="product-grid2">
@@ -50,25 +33,7 @@
             </div>
         </div>
         <!-- Modal -->
-<div id="two" class="modal fade" role="dialog">
-  <div class="modal-dialog">
 
-    <!-- Modal content-->
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Diving Cylinder</h4>
-      </div>
-      <div class="modal-body">
-        <p>Diving cylinders and Oxygen Kits</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-
-  </div>
-</div>
         <div class="col-md-4 col-sm-6">
             <div class="product-grid2">
                 <div class="product-image2"> <a href="#"> <img class="pic-1" src="/images/regulator-bcd-set.jpeg"> <img class="pic-2" src="/images/regulator-bcd-set.jpeg"> </a>
@@ -84,25 +49,7 @@
         </div>
     </div>
 <!-- Modal -->
-<div id="three" class="modal fade" role="dialog">
-  <div class="modal-dialog">
 
-    <!-- Modal content-->
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Regulator BCD set</h4>
-      </div>
-      <div class="modal-body">
-        <p>Complete dive gear </p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-
-  </div>
-</div>
 
 
     <div class="row">
@@ -120,27 +67,6 @@
             </div>
         </div>
         <!-- Modal -->
-<div id="five" class="modal fade" role="dialog">
-  <div class="modal-dialog">
-
-    <!-- Modal content-->
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Oxygen Kits</h4>
-      </div>
-      <div class="modal-body">
-        <p>Oxygen Kits</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-
-  </div>
-</div>
-
-</div>
 <section>
                <!-- Footer -->
         <footer class="page-footer font-small blue pt-4">
