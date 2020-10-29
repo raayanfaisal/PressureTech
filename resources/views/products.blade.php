815e5da04ae7ab2b3a3dd67d7cc1fe4e955d4238
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-10-29 15:47:37 +0500

    Update products.blade.php

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -48,6 +48,52 @@
             </div>
         </div>
     </div>
+        <div class="row">
+        <div class="col-md-4 col-sm-6">
+            <div class="product-grid2">
+                <div class="product-image2"> <a href="#"> <img class="pic-1" src="/images/compressor-coltri.png"> <img class="pic-2" src="/images/compressor-coltri.png"> </a>
+                    <ul class="social">
+                        <li><a href="#" data-tip="Quick View"><i class="fa fa-eye"></i></a></li>
+                        <li><a href="#" data-tip="Add to Cart"><i class="fa fa-shopping-cart"></i></a></li>
+                    </ul>
+                </div>
+                <div class="product-content">
+                <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#one">See details.</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4 col-sm-6">
+            <div class="product-grid2">
+                <div class="product-image2"> <a href="#"> <img class="pic-1" src="/images/cylinders-diving.jpg"> <img class="pic-2" src="/images/cylinders-diving.jpg"> </a>
+                    <ul class="social">
+                        <li><a href="#" data-tip="Quick View"><i class="fa fa-eye"></i></a></li>
+                        <li><a href="#" data-tip="Add to Cart"><i class="fa fa-shopping-cart"></i></a></li>
+                    </ul>
+                </div>
+                <div class="product-content">
+                <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#two">See details.</button>
+                </div>
+            </div>
+        </div>
+        <!-- Modal -->
+
+  </div>
+</div>
+        <div class="col-md-4 col-sm-6">
+            <div class="product-grid2">
+                <div class="product-image2"> <a href="#"> <img class="pic-1" src="/images/regulator-bcd-set.jpeg"> <img class="pic-2" src="/images/regulator-bcd-set.jpeg"> </a>
+                    <ul class="social">
+                        <li><a href="#" data-tip="Quick View"><i class="fa fa-eye"></i></a></li>
+                        <li><a href="#" data-tip="Add to Cart"><i class="fa fa-shopping-cart"></i></a></li>
+                    </ul>
+                </div>
+                <div class="product-content">
+                <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#three">See details.</button>
+                </div>
+            </div>
+        </div>
+    </div>
 <!-- Modal -->
 <div id="three" class="modal fade" role="dialog">
   <div class="modal-dialog">
