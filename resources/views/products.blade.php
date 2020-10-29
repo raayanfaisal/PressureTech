3ff9df010a640c89e06301bc13d5a65f0865a1b8
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-10-29 15:08:25 +0500

    Update products.blade.php

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -5,19 +5,36 @@
     <div class="row">
         <div class="col-md-4 col-sm-6">
             <div class="product-grid2">
-                <div class="product-image2"> <a href="#"> <img class="pic-1" src="/images/COLTRI COMPRESSOR MCH 13 MCH 16 SUPER SILENT EVO.png"> <img class="pic-2" src="/images/compressor-coltri.png"> </a>
+                <div class="product-image2"> <a href="#"> <img class="pic-1" src="/images/compressor-coltri.png"> <img class="pic-2" src="/images/compressor-coltri.png"> </a>
                     <ul class="social">
                         <li><a href="#" data-tip="Quick View"><i class="fa fa-eye"></i></a></li>
                         <li><a href="#" data-tip="Add to Cart"><i class="fa fa-shopping-cart"></i></a></li>
                     </ul>
                 </div>
                 <div class="product-content">
-                <a href="/compressor" class="btn-btn-primary>see more</a>
+                <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#one">See details.</button>
                 </div>
             </div>
         </div>
         <!-- Modal -->
+<div id="one" class="modal fade" role="dialog">
+  <div class="modal-dialog">
 
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">COLTRI Compressor</h4>
+      </div>
+      <div class="modal-body">
+        <p>Branded High pressure breathing air compressors</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
 </div>
         <div class="col-md-4 col-sm-6">
             <div class="product-grid2">
@@ -33,7 +50,25 @@
             </div>
         </div>
         <!-- Modal -->
+<div id="two" class="modal fade" role="dialog">
+  <div class="modal-dialog">
 
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Diving Cylinder</h4>
+      </div>
+      <div class="modal-body">
+        <p>Diving cylinders and Oxygen Kits</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
         <div class="col-md-4 col-sm-6">
             <div class="product-grid2">
                 <div class="product-image2"> <a href="#"> <img class="pic-1" src="/images/regulator-bcd-set.jpeg"> <img class="pic-2" src="/images/regulator-bcd-set.jpeg"> </a>
@@ -49,7 +84,25 @@
         </div>
     </div>
 <!-- Modal -->
+<div id="three" class="modal fade" role="dialog">
+  <div class="modal-dialog">
 
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Regulator BCD set</h4>
+      </div>
+      <div class="modal-body">
+        <p>Complete dive gear </p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
 
 
     <div class="row">
@@ -67,6 +120,27 @@
             </div>
         </div>
         <!-- Modal -->
+<div id="five" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Oxygen Kits</h4>
+      </div>
+      <div class="modal-body">
+        <p>Oxygen Kits</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+</div>
 <section>
                <!-- Footer -->
         <footer class="page-footer font-small blue pt-4">
