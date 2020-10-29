b0b3cb9fd65acfa956513f3a4b37039f5d4c1c6d
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-10-29 19:24:03 +0500

    Update products.blade.php

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -193,7 +193,7 @@
     </div>
     <!--Modal: Name-->
 
-    <a><img class="img-fluid z-depth-1" src="/images/xl-280420-175754-001-prime-line-ergo-tps-13-16-21-23-front01.jpg" alt="video"
+    <a href="/coltri"><img class="img-fluid z-depth-1" src="/images/xl-280420-175754-001-prime-line-ergo-tps-13-16-21-23-front01.jpg" alt="video"
         data-toggle="modal" data-target="#modal2"></a>
 
   </div>
