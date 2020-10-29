2be0ce69dd0954a84ab1ee2961d9ae523511922c
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-10-29 15:20:12 +0500

    Update products.blade.php

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -12,7 +12,7 @@
                     </ul>
                 </div>
                 <div class="product-content">
-                <a class="btn btn-primary btn-lg" href="/compressor">Details</a> 
+                <h2>COLTRI COMPRESSOR MCH 13/ MCH 16 SUPER SILENT EVO</h2>
                 </div>
             </div>
         </div>
