9e49ec3f49cffbcbcb3bb17dfdeaf7cf5cd0e520
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-10-29 13:08:12 +0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,13 +6,13 @@
 <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="/images/1.png" alt="First slide" width="100%" height="auto">
+      <img class="d-block w-100" src="/images/1.png" alt="First slide" width="100%" height="100%">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="/images/2.png" alt="Second slide" width="100%" height="auto">
+      <img class="d-block w-100" src="/images/2.png" alt="Second slide" width="100%" height="100%">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="/images/3.png" alt="Third slide" width="100%" height="auto">
+      <img class="d-block w-100" src="/images/3.png" alt="Third slide" width="100%" height="100%">
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
