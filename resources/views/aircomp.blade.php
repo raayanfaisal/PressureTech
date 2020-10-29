bbe2df2f97a7ee54662ce7c883bd174ddaca578e
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-10-29 18:26:59 +0500

    Add files via upload

---
+++ resources/views/aircomp.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="heading-04">
+        <h2><b>High pressure breathing air compressors
+Coltri Compact Evo</b></h2>
+        <p>The COMPACT EVO model has been designed with control systems that allow the effective monitoring of operations. The models of this line are equipped with two connections for high pressure tubes (from 232 to 330 bar), interstage pressure controls, oil level, internal cab temperature and rotation direction detector. They are equipped with three-phase motors and powerful pumping units (MCH 13, MCH 16 and MCH 18 depending on the specific model), automatic condensate drain and electric panel for management of the automatic shut-off of the compressor. The remote filling panel can be added as an option.
+</p>
+        <img src="" alt="" >
+    </div>
+</div>
+@endsection
