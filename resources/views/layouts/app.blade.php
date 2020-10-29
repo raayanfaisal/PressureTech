e0b08378c9c2dc3cd300a1fd150b143fe419fe64
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-10-29 12:12:28 +0500

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -297,7 +297,7 @@ ul#menu1 li {
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    MANDHOO DIVE
+                    PRESSURETECH
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
