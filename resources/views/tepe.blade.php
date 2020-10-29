da55f36edebd14f9c1ff7e5fd5b7f3d8b8de55fb
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-10-29 19:39:34 +0500

    Add files via upload

---
+++ resources/views/tepe.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+<img src="/images/xl-280420-175754-001-prime-line-ergo-tps-13-16-21-23-front01.jpg" alt="aircompressir" width="300px" height="200px">
+    <div class="heading-04">
+        <h2><b>Bauer PE-TE/PE-TB, 200-300l/min
+</b></h2>
+        <p>Compact, low-weight system, easily transportable
+The P 11 purification system designed especially for the PE 100 supplies pure breathing air in compliance with DIN EN 12021:20141
+Many possible applications
+Motor protection switch for PE 100-T</p>
+        <img src="" alt="" >
+    </div>
+</div>
+@endsection
