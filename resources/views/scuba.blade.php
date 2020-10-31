48c23d822e597dacf36dd67a74608fcbc3ac3ac4
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-10-31 14:33:50 +0500

    Create scuba.blade.php

---
+++ resources/views/scuba.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+<img src="/images/divegear.png" alt="aircompressir" width="300px" height="400px">
+    <div class="heading-04">
+        <h2><b>Scuba Gear Packages
+</b></h2>
+        <p>Frame: We supply branded BCD, Regulator set and other Accessories with manufacturer warranty</p>
+        <img src="" alt="" >
+    </div>
+</div>
+@endsection
