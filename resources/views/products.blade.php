f79125fd2a7295b8bafd8ae8ad21a6a5a4e21c4d
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-10-31 14:26:12 +0500

    Update products.blade.php

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -449,6 +449,56 @@
 
 </div>
 <!-- Grid row -->
+  <!-- Grid column -->
+  <div class="col-lg-4 col-md-6 mb-4">
+
+    <!--Modal: Name-->
+    <div class="modal fade" id="modal3" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-lg" role="document">
+
+        <!--Content-->
+        <div class="modal-content">
+
+          <!--Body-->
+          <div class="modal-body mb-0 p-0">
+
+            <div class="embed-responsive embed-responsive-16by9 z-depth-1-half">
+              <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/gsNY7TV4r1M"
+                allowfullscreen></iframe>
+            </div>
+
+          </div>
+
+          <!--Footer-->
+          <div class="modal-footer d-block d-md-flex justify-content-center">
+            <span class="mr-4">Spread the word!</span>
+            <a type="button" class="btn-floating btn-sm btn-fb"><i class="fab fa-facebook-f"></i></a>
+            <!--Twitter-->
+            <a type="button" class="btn-floating btn-sm btn-tw"><i class="fab fa-twitter"></i></a>
+            <!--Google +-->
+            <a type="button" class="btn-floating btn-sm btn-gplus"><i class="fab fa-google-plus-g"></i></a>
+            <!--Linkedin-->
+            <a type="button" class="btn-floating btn-sm btn-ins"><i class="fab fa-linkedin-in"></i></a>
+
+            <button type="button" class="btn btn-outline-primary btn-rounded btn-md ml-4" data-dismiss="modal">Close</button>
+
+          </div>
+
+        </div>
+        <!--/.Content-->
+
+      </div>
+    </div>
+    <!--Modal: Name-->
+
+    <a href="/images/gear"><img class="img-fluid z-depth-1" src="/images/Oxygen Kits.jpg" alt="video"
+        ></a>
+        <h5>Oxygen Kits</h5>
+  </div>
+  <!-- Grid column -->
+
+</div>
+<!-- Grid row -->
 
 
 </div>
