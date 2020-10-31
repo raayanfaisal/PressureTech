0aed5b485f9437357b46fbdf1a09944c32de7ca9
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-10-31 16:56:07 +0500

    Update rentals.blade.php

--- resources/views/rentals.blade.php
+++ resources/views/rentals.blade.php
@@ -56,53 +56,7 @@
   </div>
   <!-- Grid column -->
 
-  <!-- Grid column -->
-  <div class="col-lg-4 col-md-6 mb-4">
 
-    <!--Modal: Name-->
-    <div class="modal fade" id="modal5" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-      <div class="modal-dialog modal-lg" role="document">
-
-        <!--Content-->
-        <div class="modal-content">
-
-          <!--Body-->
-          <div class="modal-body mb-0 p-0">
-
-            <div class="embed-responsive embed-responsive-16by9 z-depth-1-half">
-              <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/PjGkVCAo8Fw"
-                allowfullscreen></iframe>
-            </div>
-
-          </div>
-
-          <!--Footer-->
-          <div class="modal-footer justify-content-center">
-            <span class="mr-4">Spread the word!</span>
-            <a type="button" class="btn-floating btn-sm btn-fb"><i class="fab fa-facebook-f"></i></a>
-            <!--Twitter-->
-            <a type="button" class="btn-floating btn-sm btn-tw"><i class="fab fa-twitter"></i></a>
-            <!--Google +-->
-            <a type="button" class="btn-floating btn-sm btn-gplus"><i class="fab fa-google-plus-g"></i></a>
-            <!--Linkedin-->
-            <a type="button" class="btn-floating btn-sm btn-ins"><i class="fab fa-linkedin-in"></i></a>
-
-            <button type="button" class="btn btn-outline-primary btn-rounded btn-md ml-4" data-dismiss="modal">Close</button>
-
-          </div>
-
-        </div>
-        <!--/.Content-->
-
-      </div>
-    </div>
-    <!--Modal: Name-->
-
-    <a href="/mark3"><img class="img-fluid z-depth-1" src="/images/mch-13-16-18-21-23-et-mark3-front01-min.jpg" alt="video"
-        ></a>
-        <h5>Coltri MCH 13-16-21-23 Mark 3</h5>
-  </div>
-  <!-- Grid column -->
 
   <!-- Grid column -->
   <div class="col-lg-4 col-md-6 mb-4">
