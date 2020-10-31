e0a7dad195752b33941231ea1b649785319d97ba
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-10-31 14:24:02 +0500

    Update gear.blade.php

--- resources/views/gear.blade.php
+++ resources/views/gear.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-<img src="/images/Diving cylinder.png" alt="aircompressir" width="300px" height="200px">
+<img src="/images/Diving cylinder.png" alt="aircompressir" width="300px" height="400px">
     <div class="heading-04">
         <h2><b>Dive gear</b></h2>
 <p>We supply branded BCD, Regulator set and other Accessories with manufacturer warranty
