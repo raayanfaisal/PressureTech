9f4a5efc58716e9c953314993ea35fa131fcfd6c
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-10-31 14:38:46 +0500

    Update smart.blade.php

--- resources/views/smart.blade.php
+++ resources/views/smart.blade.php
@@ -4,8 +4,8 @@
 <div class="container">
 <img src="/images/Coltri MCH 8-11-13-16 SMART.png" alt="aircompressir" width="300px" height="auto">
     <div class="heading-04">
-        <h2><b>Coltri MCH 8-11-13-16 SMART
-Coltri Compact Evo</b></h2>
+        <h4><b>Coltri MCH 8-11-13-16 SMART
+Coltri Compact Evo</b></h4>
 <p>Frame: Smart - Line: Efficient
 Coltri's SMART line of breathing air compressors are STANDARD line evolution, the world's best known and sold. All models are equipped with two connections for high pressure hoses (from 232 to 330 bar), a single-phase or three-phase electric motor, robust pumping units (MCH 8, MCH 13 or MCH 16 depending on the specific model) and a compact frame with handles, suitable for transport. External refilling panels and an electrical display are available as optional extras, with various automatic features including automatic condensate drainage and an autostop pressure gauge.
 </p>
