790b274dd03a28cfc22de32a23ff035e61c8540a
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-10-31 14:44:54 +0500

    Update tepe.blade.php

--- resources/views/tepe.blade.php
+++ resources/views/tepe.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
 <div class="container">
-<img src="/images/csm_pe-te-100_f5b42f50d4.jpg" alt="aircompressir" width="300px" height="400px">
+<img src="/images/csm_pe-te-100_f5b42f50d4.jpg" alt="aircompressir" width="300px" height="auto">
     <div class="heading-04">
-        <h2><b>Bauer PE-TE/PE-TB, 200-300l/min
-</b></h2>
+        <h4><b>Bauer PE-TE/PE-TB, 200-300l/min
+</b></h4>
         <p>Compact, low-weight system, easily transportable
 The P 11 purification system designed especially for the PE 100 supplies pure breathing air in compliance with DIN EN 12021:20141
 Many possible applications
