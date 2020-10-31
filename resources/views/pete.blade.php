de0733d997bbe83e2fcbf280acc5fdab6f16fb29
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-10-31 14:50:36 +0500

    Update pete.blade.php

--- resources/views/pete.blade.php
+++ resources/views/pete.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-<img src="/images/unnamed.jpg" alt="aircompressir" width="300px" height="auto">
+<img src="/images/compressr.png" alt="aircompressir" width="300px" height="auto">
     <div class="heading-04">
         <h4><b>Bauer PE-TE/PE-TB, 200-300l/min
 </b></h4>
