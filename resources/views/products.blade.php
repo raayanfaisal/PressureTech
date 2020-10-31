712933dc9b8f1dd581413ce4cfbfe0ed426c4fa4
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-10-31 15:24:48 +0500

    Update products.blade.php

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -487,7 +487,7 @@
     </div>
     <!--Modal: Name-->
 
-    <a href=""><img class="img-fluid z-depth-1" src="/images/Oxygen Kits.jpg" alt="video"
+    <a href="/oxygen"><img class="img-fluid z-depth-1" src="/images/Oxygen Kits.jpg" alt="video"
         ></a>
         <h5>Oxygen Kits</h5>
   </div>
