40eb96584a9872a8e6821c8b7da853e92047882c
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-10-31 14:06:55 +0500

    Update services.blade.php

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -13,14 +13,14 @@
     </div>
     
     <div class="services-heading">
-        <h2 class="text-center"><b>Dive Equipment Servicing</b></h2>
+        <h4 class="text-center"><b>Dive Equipment Servicing</b></h4>
         <P class="mt-5">Our in-house servicing team are ready and waiting to help with all our dive equipment servicing needs.  We service all major brand Regulators, BCDs, Dive Cylinders. 
 Equipment can be dropped off to our Service Center in Male’ City, or you can Send it to us using your preferred delivery method. If you want us to pickup your equipment, just give us a call or send us a message
  </P>
     </div>
     
     <div class="services-heading">
-        <h2 class="text-center"><b>Cylinder Inspection to European Standards -BSEN1802 & 1968</b></h2>
+        <h4 class="text-center"><b>Cylinder Inspection to European Standards -BSEN1802 & 1968</b></h4>
         <p>Visual Inspection & Hydro Testing of Diving cylinders ,Storage Cylinders ,Carbon Fiber Cylinders</p>
         <ul>
             <li>Luxfer UK Cylinders  & Luxfer USA cylinders</li>
