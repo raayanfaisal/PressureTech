fd270b885afef8c9109c7c856c7826f2b22523b4
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-10-31 14:07:56 +0500

    Update services.blade.php

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -3,24 +3,25 @@
 @section('content')
 <div class="container">
     <div class="services-heading">
-        <h4 class="text-center"><b>Servicing & maintenance of Breathing Air Compressors</b></h4>
+
         <div class="images text-center">
            <img src="/images/service.png" alt="service" width="50% height="auto">
         </div>
+        <h4 class=""><b>Servicing & maintenance of Breathing Air Compressors</b></h4>
         <P class="mt-5">Our Technicians are trained to service all the products we sell. We can service your Coltri, 
         Bauer and L&W compressor with confidence and timely manner. We are just a phone call away from your next compressor routine maintenance, 
         troubleshooting or complete overhaul. </P>
     </div>
     
     <div class="services-heading">
-        <h4 class="text-center"><b>Dive Equipment Servicing</b></h4>
+        <h4 class=""><b>Dive Equipment Servicing</b></h4>
         <P class="mt-5">Our in-house servicing team are ready and waiting to help with all our dive equipment servicing needs.  We service all major brand Regulators, BCDs, Dive Cylinders. 
 Equipment can be dropped off to our Service Center in Male’ City, or you can Send it to us using your preferred delivery method. If you want us to pickup your equipment, just give us a call or send us a message
  </P>
     </div>
     
     <div class="services-heading">
-        <h4 class="text-center"><b>Cylinder Inspection to European Standards -BSEN1802 & 1968</b></h4>
+        <h4 class=""><b>Cylinder Inspection to European Standards -BSEN1802 & 1968</b></h4>
         <p>Visual Inspection & Hydro Testing of Diving cylinders ,Storage Cylinders ,Carbon Fiber Cylinders</p>
         <ul>
             <li>Luxfer UK Cylinders  & Luxfer USA cylinders</li>
