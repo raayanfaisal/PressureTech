a1557f55bb112779c52190c3b194921bc1b2db28
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-10-31 14:24:48 +0500

    Update tepe.blade.php

--- resources/views/tepe.blade.php
+++ resources/views/tepe.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-<img src="/images/csm_pe-te-100_f5b42f50d4.jpg" alt="aircompressir" width="300px" height="200px">
+<img src="/images/csm_pe-te-100_f5b42f50d4.jpg" alt="aircompressir" width="300px" height="400px">
     <div class="heading-04">
         <h2><b>Bauer PE-TE/PE-TB, 200-300l/min
 </b></h2>
