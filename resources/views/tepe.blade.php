ee7a51ee65252606b9c955b315e81b1cae3e260c
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-10-31 14:56:25 +0500

    Update tepe.blade.php

--- resources/views/tepe.blade.php
+++ resources/views/tepe.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
 <img src="/images/compressor-bauer-posseidon-pe100-eletrico-500x500.jpg" alt="aircompressir" width="500px" height="auto">
     <div class="heading-04">
-        <h4><b>Bauer PE-TE/PE-TB, 200-300l/min
+        <h4><b>Bauer PE100, 100l/min
 </b></h4>
         <p>Compact, low-weight system, easily transportable
 The P 11 purification system designed especially for the PE 100 supplies pure breathing air in compliance with DIN EN 12021:20141
