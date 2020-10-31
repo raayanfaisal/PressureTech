1c81356313a4162fb4419fe3f20d34e7795431a8
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-10-31 15:11:27 +0500

    Update p200.blade.php

--- resources/views/p200.blade.php
+++ resources/views/p200.blade.php
@@ -4,14 +4,14 @@
 <div class="container">
 <img src="/images/p200edited.png" alt="aircompressir" width="500px" height="auto">
     <div class="heading-04">
-        <h4><b>Coltri MCH 13-16-21-23 Mark 3
+        <h4><b>Bauer PE-TE/PE-TB, 200-300l/min
 </b></h4>
-        <p>Frame: Mark 3 TPS - Line: Prime
-The transportable compressor units with optimised equipment for use in vehicles and on ships
+        <p>The transportable compressor units with optimised equipment for use in vehicles and on ships
 Uncompromising on quality: With a robust compressor block and piston rings made from wear-resistant special plastic
 Durable and robust: Thanks to oil cooling in the final compressor stage. For low wear and long service life
 Ideal for use at sea: Intercoolers and aftercoolers made from rust-free stainless steel. For high corrosion resistance
 Reliable lubrication: Thanks to low-pressure oil pump with oil filter. For effective lubrication and long oil change intervals
+
 </p>
         <img src="" alt="" >
     </div>
