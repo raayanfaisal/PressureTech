3828cc9624c318aaa7ea5274d8b522a5b3015b51
raayanfaisal <64302831+raayanfaisal@users.noreply.github.com>
2020-10-31 17:07:12 +0500

    Update cylinder.blade.php

--- resources/views/cylinder.blade.php
+++ resources/views/cylinder.blade.php
@@ -5,7 +5,7 @@
 <img src="/images/Diving cylinders.jpg" alt="aircompressir" width="300px" height="auto">
     <div class="heading-04">
         <h4><b>Diving cylinders</b></h4>
-<p>We offer steel and aluminium cylinders  and Valves at a competitive price
+<p>We offer steel and aluminium cylinders 
 </p>
 
         <img src="" alt="" >
